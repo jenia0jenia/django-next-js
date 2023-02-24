c522c69f523f9ee38e195edc15f9ac0446d465f5
jenia0jenia <jenia0jenia@gmail.com>
2023-02-24 15:41:08 +0300

    version shoud be integer

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -88,9 +88,6 @@ DATABASES = {
         'PORT': os.environ.get('PORT'),
     }
 }
-
-print(DATABASES)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
